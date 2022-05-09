87f60f77eb402781222c68630aa8501dfd3b8295
NicoUmansky <umanskynico@gmail.com>
2022-05-09 12:04:13 -0300

    Terminado

---
+++ Assets/Scripts/Instantiator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject ObjectToClone;
+    public int canti;
+    public void CloneObject()
+    {
+        int i = 0;
+        while (i < canti)
+        {
+            Instantiate(ObjectToClone);
+            i++;
+        }
+   
+    }
+}
